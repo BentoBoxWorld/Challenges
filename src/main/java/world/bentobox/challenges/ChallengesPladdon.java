7c2be91be97ced65acb14be1fdd493998fb48bfe
BONNe <bonne@bonne.id.lv>
2021-08-14 21:42:45 +0300

    Switch to annotations instead of plugin.yml file.

--- src/main/java/world/bentobox/challenges/ChallengesPladdon.java
+++ src/main/java/world/bentobox/challenges/ChallengesPladdon.java
@@ -7,12 +7,19 @@
 package world.bentobox.challenges;
 
 
+import org.bukkit.plugin.java.annotation.dependency.Dependency;
+import org.bukkit.plugin.java.annotation.plugin.ApiVersion;
+import org.bukkit.plugin.java.annotation.plugin.Plugin;
+
 import world.bentobox.bentobox.api.addons.Addon;
 import world.bentobox.bentobox.api.addons.Pladdon;
 
 /**
  * @author tastybento
  */
+@Plugin(name="Pladdon", version="1.0")
+@ApiVersion(ApiVersion.Target.v1_17)
+@Dependency(value = "BentoBox")
 public class ChallengesPladdon extends Pladdon
 {
     @Override
