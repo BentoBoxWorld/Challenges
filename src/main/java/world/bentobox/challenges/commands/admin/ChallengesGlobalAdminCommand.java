5f4fc315e64717ec4c572ac8be29ca2c259d81a8
BONNe <bonne@bonne.id.lv>
2021-09-19 21:48:19 +0300

    Fix permission link.

--- src/main/java/world/bentobox/challenges/commands/admin/ChallengesGlobalAdminCommand.java
+++ src/main/java/world/bentobox/challenges/commands/admin/ChallengesGlobalAdminCommand.java
@@ -35,7 +35,7 @@ public class ChallengesGlobalAdminCommand extends CompositeCommand
 	@Override
 	public void setup()
 	{
-		this.setPermission("admin.challenges");
+		this.setPermission("addon.admin.challenges");
 		this.setParametersHelp("challenges.commands.admin.main.parameters");
 		this.setDescription("challenges.commands.admin.main.description");
 	}
