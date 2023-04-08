ad5423de823c382bc41148516d4689bc30c0d729
BONNe <bonne@bonne.id.lv>
2023-04-08 18:30:29 +0300

    Update ChallengesPladdon.java

--- src/main/java/world/bentobox/challenges/ChallengesPladdon.java
+++ src/main/java/world/bentobox/challenges/ChallengesPladdon.java
@@ -7,17 +7,12 @@
 package world.bentobox.challenges;
 
 
-import org.bukkit.plugin.java.annotation.plugin.ApiVersion;
-import org.bukkit.plugin.java.annotation.plugin.Plugin;
-
 import world.bentobox.bentobox.api.addons.Addon;
 import world.bentobox.bentobox.api.addons.Pladdon;
 
 /**
  * @author tastybento
  */
-@Plugin(name="Challenges", version="1.0")
-@ApiVersion(ApiVersion.Target.v1_17)
 public class ChallengesPladdon extends Pladdon
 {
     @Override
